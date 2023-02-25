65e38e840d02eb5f49f33336ee5985d19fac33f3
Gabriel Pedroso <gabriel.pedroso@masterlink.pt>
2023-02-25 13:52:49 +0000

    Adding diet simulator and updating server request

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,7 @@ import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
     organization: "org-OvzI9SnUXficJ9icnhlhbzzP",
-    apiKey: "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",
+    apiKey: "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",
 });
 
 const openai = new OpenAIApi(configuration);
@@ -21,10 +21,27 @@ app.use(cors());
 
 app.post('/', async (req, res) => {
     try {
-        const { message } = req.body
+        const prompt = `Responda como um nutricionista. 
+        Primeiramente, calcule meu TDEE  baseado nos seguintes dados: 
+        Sou ${req.body.providerGender}, vivo no Brasil, tenho ${req.body.providerAge} anos, 
+        peso ${req.body.providerWeight}kgs, altura ${req.body.providerHeight}cm e 
+        nível de atividade física ${req.body.providerActivityLevel} 
+        
+        Com este valor do TDEE, monte um plano de dieta para ${req.body.dietFrequency} dias e 
+        ${req.body.mealFrequency} refeições por dia, com o objetivo de ${req.body.weightObjective}. 
+        Inicialmente apresente o "Consumo médio diário" em(kcal / dia), sendo este o valor 
+        de consumo calórico diário referente a esta dieta. Apresente as porções em medidas
+        (exemplo: 200g de carne magra, 50g de nozes, 150g de arroz integral, 50g de mirtilos). 
+        Não utilize "xícara" e nem "colher" como valor de medido, utilize seu peso(em gramas) 
+        Não apresente "1 porção de legumes", apresente qual o legume é recomendado e 
+        sua quantidade(em gramas), exemplo: "200g de brócolis". 
+        
+        Apresente o valor em HTML5 e cada "Dia" contendo uma classe "font-bold", 
+        contendo as refeições de todos os 5 dias, para facilitar a leitura: `
+
         const response = await openai.createCompletion({
             model: "text-davinci-003",
-            prompt: message,
+            prompt: prompt,
             max_tokens: 1650,
             temperature: 0,
             frequency_penalty: 0,
